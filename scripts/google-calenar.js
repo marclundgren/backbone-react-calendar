abbcfeea89fb7a2b9ff3b1fa839e1564b606a170
Marc Lundgren <marclundgren2.0@gmail.com>
2014-09-12 16:58:54 -0700

    temp

--- scripts/google-calenar.js
+++ scripts/google-calenar.js
@@ -2,9 +2,76 @@
  * @jsx React.DOM
  */
 
-var GoogleCalendar = React.createClass({displayName: 'GoogleCalendar',});
+var converter = new Showdown.converter();
 
-var GoogleEvent = React.createClass({displayName: 'GoogleEvent',});
+// data: Google CalendarEvents
+
+var GoogleCalendar = React.createClass({displayName: 'GoogleCalendar',
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: this.props.url,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
+  render: function() {
+    // todo
+  }
+});
+
+var GoogleEvent = React.createClass({displayName: 'GoogleEvent',
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      React.DOM.div({className: "comment"}, 
+        React.DOM.h2({className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.DOM.span({dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
+
+var GoogleEventList = React.createClass({displayName: 'GoogleEventList',
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment, index) {
+      return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        Comment({author: comment.author, key: index}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.DOM.div({className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+React.renderComponent(
+  GoogleCalendar({url: "events.json", dataType: "json"}),
+  document.getElementById('calendar')
+);
 
 // eventsSource: '//www.google.com/calendar/feeds/vdmtdcektajkqjk51vvda4ni4k%40group.calendar.google.com/public/full?alt=json-in-script&singleevents=true&max-results=9999&futureevents=false&orderby=starttime&sortorder=ascending',
 
