25a924779b4a43fb063fee3f50e6f365b14cc947
Marc Lundgren <marclundgren2.0@gmail.com>
2014-09-12 16:33:18 -0700

    adding google calendar js

---
+++ scripts/google-calenar.js
@@ -0,0 +1,34 @@
+/**
+ * @jsx React.DOM
+ */
+
+var GoogleCalendar = React.createClass({displayName: 'GoogleCalendar',});
+
+var GoogleEvent = React.createClass({displayName: 'GoogleEvent',});
+
+// eventsSource: '//www.google.com/calendar/feeds/vdmtdcektajkqjk51vvda4ni4k%40group.calendar.google.com/public/full?alt=json-in-script&singleevents=true&max-results=9999&futureevents=false&orderby=starttime&sortorder=ascending',
+
+// day: '2014-06-12',
+
+/*
+$.ajax({
+  cache: true,
+  dataType: self.options.eventsDataType,
+  url:      buildEventsUrl(source, params)
+}).done(function(response) {
+  events = _.map(response.feed.entry, function(item) {
+    return {
+      class: 'event-warning',
+      title: item.title.$t,
+      id: item.gCal$uid.value,
+      start: String(Date.parse(item.gd$when[0].startTime)),
+      end: String(Date.parse(item.gd$when[0].endTime)),
+      url: item.link[0].href
+    };
+  });
+
+  self.options.events = events;
+
+  self._render();
+});
+*/
