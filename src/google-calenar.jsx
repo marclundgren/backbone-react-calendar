5783d1d79c37493f8f23ae33e01e8a05e3ef415f
Marc <marclundgren2.0@gmail.com>
2014-09-12 16:58:54 -0700

    temp

--- src/google-calenar.jsx
+++ src/google-calenar.jsx
@@ -2,9 +2,76 @@
  * @jsx React.DOM
  */
 
-var GoogleCalendar = React.createClass({});
+var converter = new Showdown.converter();
 
-var GoogleEvent = React.createClass({});
+// data: Google CalendarEvents
+
+var GoogleCalendar = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: this.props.url,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
+  render: function() {
+    // todo
+  }
+});
+
+var GoogleEvent = React.createClass({
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
+var GoogleEventList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment, index) {
+      return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        <Comment author={comment.author} key={index}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+React.renderComponent(
+  <GoogleCalendar url="events.json" dataType="json" />,
+  document.getElementById('calendar')
+);
 
 // eventsSource: '//www.google.com/calendar/feeds/vdmtdcektajkqjk51vvda4ni4k%40group.calendar.google.com/public/full?alt=json-in-script&singleevents=true&max-results=9999&futureevents=false&orderby=starttime&sortorder=ascending',
 
