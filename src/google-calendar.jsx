7defed75e2147f4e1f74af7456f8e70152528584
Marc <marclundgren2.0@gmail.com>
2014-09-12 17:14:09 -0700

    more temp changes

--- src/google-calendar.jsx
+++ src/google-calendar.jsx
@@ -7,39 +7,18 @@ var converter = new Showdown.converter();
 // data: Google CalendarEvents
 
 var GoogleCalendar = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-
-  loadCommentsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: this.props.url,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-
-  componentDidMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-
   render: function() {
     // todo
   }
 });
 
+// for now, assume Timed Event
 var GoogleEvent = React.createClass({
   render: function() {
     var rawMarkup = converter.makeHtml(this.props.children.toString());
     return (
-      <div className="comment">
-        <h2 className="commentAuthor">
+      <div className="data">
+        <h2 className="dataAuthor">
           {this.props.author}
         </h2>
         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
@@ -49,20 +28,42 @@ var GoogleEvent = React.createClass({
 });
 
 var GoogleEventList = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadDataFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: this.props.url,
+      success: function(data) {
+        // When the state is updated, the component re-renders itself.
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadDataFromServer();
+  },
+
   render: function() {
-    var commentNodes = this.props.data.map(function(comment, index) {
+    var dataNodes = this.props.data.map(function(data, index) {
       return (
         // `key` is a React-specific concept and is not mandatory for the
         // purpose of this tutorial. if you're curious, see more here:
         // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
-        <Comment author={comment.author} key={index}>
-          {comment.text}
-        </Comment>
+        <GoogleEvent title={data.title} key={index}>
+          {data.text}
+        </GoogleEvent>
       );
     });
     return (
-      <div className="commentList">
-        {commentNodes}
+      <div className="dataList">
+        {dataNodes}
       </div>
     );
   }
