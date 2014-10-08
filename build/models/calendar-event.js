b23a3d3687ef60e065b2ecb02d237e508dcfaf24
Marc Lundgren <marclundgren2.0@gmail.com>
2014-10-08 09:51:02 -0700

    build calendar event. this should not be breaking anything

--- build/models/calendar-event.js
+++ build/models/calendar-event.js
@@ -6,12 +6,15 @@ Backbone.CalendarEvent = Backbone.Model.extend({
     calendar:     '',
     content:      '',
     date:         '',
+    endTime:      '',
+    endMoment:    moment(), // alternatively, i could create a duration method and and endTime method...since i dont need to search against them. yeah do that...
     month:        '',
     year:         '',
     endTime:      '',
     id:           '',
     link:         '',
     startTime:    '',
+    startMoment:  moment(),
     title:        '',
     updated:      '',
     location:     '',
@@ -28,10 +31,20 @@ Backbone.CalendarEvent = Backbone.Model.extend({
     // this.set('yearMonthDay', startMoment.format('YYYY-MM-DD'));
   },
 
+  ends: function() {
+    var endMoment = this._getEndMoment();
+
+    return endMoment.format('YYYY MMMM DD hh:mm a');
+  },
+
   starts: function() {
     var startMoment = this._getStartMoment();
 
     return startMoment.format('YYYY MMMM DD hh:mm a');
+  },
+
+  _getEndMoment: function() {
+    return moment(this.get('endTime'));
   },
 
   _getStartMoment: function() {
@@ -40,7 +53,7 @@ Backbone.CalendarEvent = Backbone.Model.extend({
 
   duration: function() {
     var startMoment = this._getStartMoment();
-    var endMoment = moment(this.get('endTime'));
+    var endMoment = this._getEndMoment();
 
     var unit = 'hours';
 
